a137bde95c93208712010ef97069a1c615a81264
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2016-10-25 14:02:02 +0200

    Do not require js (does not work with municipio async)

--- dist/js/CustomerFeedback.dev.js
+++ dist/js/CustomerFeedback.dev.js
@@ -12,38 +12,42 @@ CustomerFeedback.Form = (function ($) {
         $('[data-action="customer-feedback-submit-response"]').on('click', function (e) {
             e.preventDefault();
 
-            $(e.target).html('<i class="fa fa-spinner fa-spin"></i>');
+            $target = $(e.target).parents('.customer-feedback-container');
+            $(e.target).html('<i class="spinner spinner-dark"></i>');
 
-            var responsePostId = $('#customer-feedback-post-id').val();
+            var responsePostId = $target.find('[name="customer-feedback-post-id"]').val();
             var responseValue = $(e.target).val();
 
-            this.submitInitialResponse(responsePostId, responseValue);
+            this.submitInitialResponse($target, responsePostId, responseValue);
         }.bind(this));
 
         // Comment submit click
         $('[data-action="customer-feedback-submit-comment"]').on('click', function (e) {
             e.preventDefault();
 
-            $('#customer-feedback-comment-text').removeClass('invalid');
+            $(e.target).html('<i class="spinner spinner-dark"></i>');
+            $target = $(e.target).parents('.customer-feedback-container');
+
+            $target.find('[name="customer-feedback-comment-text"]').removeClass('invalid');
 
             var commentType = 'comment';
-            var answerId = $('#customer-feedback-answer-id').val();
-            var postId = $('#customer-feedback-post-id').val();
-            var comment = $('#customer-feedback-comment-text').val();
-            var email = $('#customer-feedback-comment-email').val();
+            var answerId = $target.find('[name="customer-feedback-answer-id"]').val();
+            var postId = $target.find('[name="customer-feedback-post-id"]').val();
+            var comment = $target.find('[name="customer-feedback-comment-text"]').val();
+            var email = $target.find('[name="customer-feedback-comment-email"]').val();
 
             if (comment.length === 0) {
-                $('#customer-feedback-comment-text').addClass('invalid');
-                $('#customer-feedback-comment-text').after('<div class="clearfix"></div><div style="margin-top: 5px;" class="notice notice-sm danger">You need to write a comment before posting it.</div>');
+                $target.find('[name="customer-feedback-comment-text"]').addClass('invalid');
+                $target.find('[name="customer-feedback-comment-text"]').after('<div class="clearfix"></div><div style="margin-top: 5px;" class="notice notice-sm danger">You need to write a comment before posting it.</div>');
                 return false;
             }
 
-            this.submitComment(answerId, postId, commentType, comment, email);
+            this.submitComment($target, answerId, postId, commentType, comment, email);
 
         }.bind(this));
     };
 
-    Form.prototype.submitComment = function (answerId, postId, commentType, comment, email) {
+    Form.prototype.submitComment = function (target, answerId, postId, commentType, comment, email) {
         var data = {
             action: 'submit_comment',
             postid: postId,
@@ -53,10 +57,12 @@ CustomerFeedback.Form = (function ($) {
             email: email
         };
 
+        var $target = target;
+
         $.post(ajaxurl, data, function (response) {
             if (response == 'true') {
-                $('#customer-feedback-comment').remove();
-                $('#customer-feedback-thanks').show();
+                $target.find('.customer-feedback-comment').remove();
+                $target.find('.customer-feedback-thanks').show();
             }
         });
     };
@@ -67,7 +73,9 @@ CustomerFeedback.Form = (function ($) {
      * @param  {string}  answer Yes or no
      * @return {void}
      */
-    Form.prototype.submitInitialResponse = function (postId, answer) {
+    Form.prototype.submitInitialResponse = function (target, postId, answer) {
+        var $target = target;
+
         var data = {
             action: 'submit_response',
             postid: postId,
@@ -76,15 +84,15 @@ CustomerFeedback.Form = (function ($) {
 
         $.post(ajaxurl, data, function (response) {
             if (data.answer == 'yes' && !isNaN(parseFloat(response)) && isFinite(response)) {
-                $('#customer-feedback-answers').hide();
-                $('#customer-feedback-thanks').show();
+                $('.customer-feedback-container').find('.customer-feedback-answers').hide();
+                $('.customer-feedback-container').find('.customer-feedback-thanks').show();
             }
 
             if (data.answer == 'no' && !isNaN(parseFloat(response)) && isFinite(response)) {
-                $('#customer-feedback-post-id').after('<input type="hidden" id="customer-feedback-answer-id" value="' + response + '">');
+                $target.find('[name="customer-feedback-post-id"]').after('<input type="hidden" name="customer-feedback-answer-id" value="' + response + '">');
 
-                $('#customer-feedback-answers').remove();
-                $('#customer-feedback-comment').show();
+                $target.find('.customer-feedback-answers').remove();
+                $target.find('.customer-feedback-comment').show();
             }
         });
     };
--- templates/form.php
+++ templates/form.php
@@ -1,5 +1,6 @@
-<div class="box box-index creamy gutter hidden-print" id="customer-feedback">
-    <input type="hidden" id="customer-feedback-post-id" value="<?php echo get_the_id(); ?>">
+<?php $num = isset($num) && $num > 0 ? $num++ : 1; ?>
+<div class="box box-index creamy gutter hidden-print customer-feedback-container">
+    <input type="hidden" name="customer-feedback-post-id" value="<?php echo get_the_id(); ?>">
 
     <div class="grid">
         <div class="grid-xs-12">
@@ -9,25 +10,25 @@
         <div class="grid-xs-12">
             <div class="gutter gutter-top gutter-sm">
                 <?php if (!isset($_COOKIE['customer-feedback']) || !in_array(get_the_id(), unserialize(stripslashes($_COOKIE['customer-feedback'])))) : ?>
-                <div id="customer-feedback-answers">
+                <div class="customer-feedback-answers">
                     <button rel="nofollow" class="btn btn-success" value="yes" data-action="customer-feedback-submit-response"><?php _e('Yes'); ?></button>
                     <button rel="nofollow" class="btn btn-error" value="no" data-action="customer-feedback-submit-response"><?php _e('No'); ?></a>
                 </div>
-                <div id="customer-feedback-comment" class="text-left gutter gutter-top gutter-sm" style="display: none;">
+                <div class="customer-feedback-comment text-left gutter gutter-top gutter-sm" style="display: none;">
                     <div class="form-group">
-                        <label for="customer-feedback-comment-text"><?php echo $commentLabel; ?></label>
-                        <textarea class="form-control" style="height: 100px;" id="customer-feedback-comment-text" name="customer-feedback-comment-text"></textarea>
+                        <label for="customer-feedback-comment-text-<?php echo $num; ?>"><?php echo $commentLabel; ?></label>
+                        <textarea class="form-control" style="height: 100px;" id="customer-feedback-comment-text-<?php echo $num; ?>" name="customer-feedback-comment-text"></textarea>
                     </div>
                     <div class="form-group">
-                        <label for="customer-feedback-comment-email"><?php echo $emailLabel; ?> (<?php _e('Optional', 'customer-feedback'); ?>)</label>
+                        <label for="customer-feedback-comment-email-<?php echo $num; ?>"><?php echo $emailLabel; ?> (<?php _e('Optional', 'customer-feedback'); ?>)</label>
                         <small class="block-level"><?php echo $emailExplain; ?></small>
-                        <input type="email" id="customer-feedback-comment-email" name="customer-feedback-comment-email" value="<?php echo $userEmail; ?>">
+                        <input type="email" id="customer-feedback-comment-email-<?php echo $num; ?>" name="customer-feedback-comment-email" value="<?php echo $userEmail; ?>">
                     </div>
                     <div class="form-group">
                         <button rel="nofollow" style="margin-top:5px;" class="btn btn-submit" data-action="customer-feedback-submit-comment"><?php _e('Send', 'customer-feedback'); ?></button>
                     </div>
                 </div>
-                <div id="customer-feedback-thanks" style="display: none;">
+                <div class="customer-feedback-thanks" style="display: none;">
                     <div class="notice success"><?php echo $thanksText; ?></div>
                 </div>
                 <?php else : ?>
