d905d076ae1868f172f43a5bce2cebf6e1d410df
Kristoffer Svanmark <kristoffer.svanmark@knowit.se>
2017-05-22 09:11:33 +0200

    Removes feedback forwarding metabox from non-activated posttypes

--- source/php/App.php
+++ source/php/App.php
@@ -8,11 +8,24 @@ class App
     {
         add_action('wp_enqueue_scripts', array($this, 'enqueueScripts'));
         add_action('admin_enqueue_scripts', array($this, 'adminEnqueue'));
+        add_action('add_meta_boxes', array($this, 'removeUnwantedModuleMetaboxes'));
 
         new Responses();
         new Summary();
         new Form();
         new Options();
+    }
+
+    public function removeUnwantedModuleMetaboxes($postType)
+    {
+        $allowedPostTypes = apply_filters('CustomerFeedback/post_types', get_field('customer_feedback_posttypes', 'option'));
+        if (is_null($allowedPostTypes)) {
+            $allowedPostTypes = array('page');
+        }
+
+        if (!in_array($postType, $allowedPostTypes)) {
+            remove_meta_box('acf-group_591c10ab88d77', $postType, 'side');
+        }
     }
 
     /**
