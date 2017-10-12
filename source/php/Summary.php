a7554b1637b6ba25ec9ef86779ddaf186e71fb26
Jonatan Hanson <jonatan.hanson@hiq.se>
2017-10-12 17:40:07 +0200

    Changed cron running time

--- source/php/Summary.php
+++ source/php/Summary.php
@@ -42,7 +42,7 @@ class Summary
             wp_clear_scheduled_hook('customer-feedback/email_summary', array($summary['email_address'], 'monthly'));
 
             wp_schedule_event(
-                strtotime('tomorrow midnight -2 hour'),
+                strtotime('tomorrow midnight -3 hour'),
                 $summary['interval'],
                 'customer-feedback/email_summary',
                 array(
