55ba1317c6f69455ec74058adb889c3967fdeff4
Joel Bernerman <joel.bernerman@helsingborg.se>
2020-12-16 10:08:10 +0100

    Make github action workflow use php 7.4

---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-3.0/develop',
+    'version' => 'dev-3.0/develop',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '88a019692b8c6cf6a41e16c0ad8ceb2ee94f361d',
+    'name' => 'helsingborg-stad/customer-feedback',
+  ),
+  'versions' => 
+  array (
+    'helsingborg-stad/acf-export-manager' => 
+    array (
+      'pretty_version' => '1.0.11',
+      'version' => '1.0.11.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '27e904a24eb013cac5c8f3d3c30e88b5b9b7859b',
+    ),
+    'helsingborg-stad/customer-feedback' => 
+    array (
+      'pretty_version' => 'dev-3.0/develop',
+      'version' => 'dev-3.0/develop',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '88a019692b8c6cf6a41e16c0ad8ceb2ee94f361d',
+    ),
+  ),
+);
