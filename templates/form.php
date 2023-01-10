094e33c3a6eed6b2baa629de2567df558e5be4a1
Thor Brink <thor.brink@helsingborg.se>
2023-01-10 11:22:37 +0000

    Add aria-hidden values to fix a11y text-clipped warnings

--- templates/form.php
+++ templates/form.php
@@ -5,7 +5,7 @@ $cardStyle = get_theme_mod('flat_ui_design') ? "c-card--flat u-padding--1" : "u-
 <div id="customer-feedback"
     class="customer-feedback-container c-card <?php echo($cardStyle);?> ">
     <h2 id="feedback" class="c-typography c-typography__variant--h3">
-        <span aria-hidden class="c-icon c-icon--color-primary c-icon--size-inherit material-icons" aria-hidden>
+        <span aria-hidden="true" class="c-icon c-icon--color-primary c-icon--size-inherit material-icons">
             question_answer
         </span>
         <?php echo $mainQuestion ?>
@@ -25,7 +25,7 @@ $cardStyle = get_theme_mod('flat_ui_design') ? "c-card--flat u-padding--1" : "u-
             <div class="customer-feedback-js-error u-margin__bottom--3 u-margin__top--3" style="display: none;">
                 <div id="" class="c-notice c-notice--danger">
                     <span class="c-notice__icon">
-                        <i aria-hidden id="" class="c-icon c-icon--color- c-icon--size-md material-icons">
+                        <i aria-hidden="true" id="" class="c-icon c-icon--color- c-icon--size-md material-icons">
                             error_outline
                         </i>
                     </span>
@@ -40,7 +40,7 @@ $cardStyle = get_theme_mod('flat_ui_design') ? "c-card--flat u-padding--1" : "u-
                 aria-pressed="false" type="button" value="yes" data-action="customer-feedback-submit-response">
                 <span class="c-button__label">
                     <span class="c-button__label-text">
-                        <i aria-hidden class="c-icon c-icon--color-primary c-icon--size-inherit material-icons">
+                        <i aria-hidden="true" class="c-icon c-icon--color-primary c-icon--size-inherit material-icons">
                             thumb_up
                         </i>
                         <?php _e('Yes', 'customer-feedback'); ?>
@@ -52,7 +52,7 @@ $cardStyle = get_theme_mod('flat_ui_design') ? "c-card--flat u-padding--1" : "u-
                 aria-pressed="false" type="button" value="no" data-action="customer-feedback-submit-response">
                 <span class="c-button__label">
                     <span class="c-button__label-text">
-                        <i aria-hidden class="c-icon c-icon--color-primary c-icon--size-inherit material-icons">
+                        <i aria-hidden="true" class="c-icon c-icon--color-primary c-icon--size-inherit material-icons">
                             thumb_down
                         </i>
                         <?php _e('No', 'customer-feedback'); ?>
@@ -179,7 +179,7 @@ $cardStyle = get_theme_mod('flat_ui_design') ? "c-card--flat u-padding--1" : "u-
                 aria-pressed="false" type="button" value="send" data-action="customer-feedback-submit-comment">
                 <span class="c-button__label">
                     <span class="c-button__label-text">
-                        <i aria-hidden class="c-icon c-icon--color-white c-icon--size-inherit material-icons">
+                        <i aria-hidden="true" class="c-icon c-icon--color-white c-icon--size-inherit material-icons">
                             send
                         </i>
                         <?php _e('Send', 'customer-feedback'); ?>
@@ -192,7 +192,7 @@ $cardStyle = get_theme_mod('flat_ui_design') ? "c-card--flat u-padding--1" : "u-
         <div id="customer-feedback-thanks" class="customer-feedback-thanks" style="display: none;">
             <div class="c-notice c-notice--success u-margin__top--2">
                 <span class="c-notice__icon">
-                    <i aria-hidden class="c-icon c-icon--color- c-icon--size-md material-icons">
+                    <i aria-hidden="true" class="c-icon c-icon--color- c-icon--size-md material-icons">
                         check
                     </i>
                 </span>
@@ -206,7 +206,7 @@ $cardStyle = get_theme_mod('flat_ui_design') ? "c-card--flat u-padding--1" : "u-
         <div id="customer-feedback-error" class="customer-feedback-error" style="display: none;">
             <div id="" class="c-notice c-notice--danger u-margin__top--2">
                 <span class="c-notice__icon">
-                    <i aria-hidden id="" class="c-icon c-icon--color- c-icon--size-md material-icons">
+                    <i aria-hidden="true" id="" class="c-icon c-icon--color- c-icon--size-md material-icons">
                         error_outline
                     </i>
                 </span>
@@ -222,7 +222,7 @@ $cardStyle = get_theme_mod('flat_ui_design') ? "c-card--flat u-padding--1" : "u-
         <div class="customer-feedback-already-submitted">
             <div class="c-notice c-notice--info u-margin__top--2">
                 <span class="c-notice__icon">
-                    <i aria-hidden id="" class="c-icon c-icon--color- c-icon--size-md material-icons">
+                    <i aria-hidden="true" id="" class="c-icon c-icon--color- c-icon--size-md material-icons">
                         error_outline
                     </i>
                 </span>
