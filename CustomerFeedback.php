5e9c2ae045457446d1244af2d0294470bfd475da
GitHub Action <>
2025-03-25 12:45:43 +0000

    build: update package file versions to 4.1.0

--- CustomerFeedback.php
+++ CustomerFeedback.php
@@ -4,7 +4,7 @@
  * Plugin Name:       Customer Feedback
  * Plugin URI:        https://github.com/helsingborg-stad/Customer-feedback
  * Description:       Puts a customer feedback form on each page
- * Version: 4.0.12
+ * Version: 4.1.0
  * Author:            Kristoffer Svanmark
  * Author URI:        https://github.com/helsingborg-stad
  * License:           MIT
